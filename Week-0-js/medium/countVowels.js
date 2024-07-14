9f6ca5120b47f426497071376d31ba1d0b5760df
yashksaini-coder <ys3853428@gmail.com>
2024-07-14 21:26:03 +0530

    chore: Refactor countVowels function for improved efficiency

--- Week-0-js/medium/countVowels.js
+++ Week-0-js/medium/countVowels.js
@@ -6,7 +6,17 @@
 */
 
 function countVowels(str) {
-    // Your code here
+  let vowels = 'aeiouAEIOU';
+  let count = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (vowels.includes(str[i])) {
+      count++;
+    }
+  }
+  return count;
 }
 
 module.exports = countVowels;
+
+str = 'yash kumar saini';
+console.log(countVowels(str)); // 1
