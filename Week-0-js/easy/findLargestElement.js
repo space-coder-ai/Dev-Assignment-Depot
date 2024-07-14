c7dfc6ab56bbd44f1d0241e3711abaa60f2231da
yashksaini-coder <ys3853428@gmail.com>
2024-07-14 21:03:36 +0530

    feat: Add logic to find the largest element in an array

--- Week-0-js/easy/findLargestElement.js
+++ Week-0-js/easy/findLargestElement.js
@@ -6,7 +6,16 @@
 */
 
 function findLargestElement(numbers) {
-    
+    let biggest = numbers[0];
+    for (let i = 0; i < numbers.length; i++) {
+        if (numbers[i] > biggest) {
+            biggest = numbers[i];
+        }
+    }
+    return biggest;
 }
 
 module.exports = findLargestElement;
+
+nums = [3, 7, 2, 9, 1];
+console.log(findLargestElement(nums));
